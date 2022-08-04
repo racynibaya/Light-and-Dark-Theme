df70598bca6b1072ff3f04a472f56cce35509aa5
Racyn Ibaya <racynibaya@outlook.com>
2022-08-04 11:00:48 -0400

    adding light and dark mode

--- script.js
+++ script.js
@@ -6,10 +6,26 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('textbox');
 
+const darkMode = function () {
+  document.documentElement.setAttribute('data-theme', 'dark');
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = 'Dark mode';
+  toggleIcon.children[1].classList.remove('fa-sun');
+  toggleIcon.children[1].classList.add('fa-moon');
+};
+
+const lightMode = function () {
+  document.documentElement.setAttribute('data-theme', 'light');
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = 'Light mode';
+  toggleIcon.children[1].classList.remove('fa-moon');
+  toggleIcon.children[1].classList.add('fa-sun');
+};
+
 const changeTheme = function (e) {
-  e.target.checked
-    ? document.documentElement.setAttribute('data-theme', 'dark')
-    : document.documentElement.setAttribute('data-theme', 'light');
+  e.target.checked ? darkMode() : lightMode();
 };
 
 toggleSwitch.addEventListener('change', changeTheme);
